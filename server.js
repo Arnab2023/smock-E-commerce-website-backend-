41dfd8a3518e3e4c634e03c98a32351bb872f82f
Arnab2023 <arnab192023@gmail.com>
2024-04-12 21:52:54 +0530

    Added cors option

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require("http");
 const app = require("./app");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: [
+    "https://smock-e-commerce-website-frontend.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://ecommerce-frontend-opal-sigma.vercel.app",
+  ],
+};
+
+app.use(cors(corsOptions));
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Ecommerce Backend API started on PORT NO.: ${port}`);
